f00507ed895bcdcc291b8c16335f942b90012c6d
胡雪霏 <1196445339@qq.com>
2020-07-08 16:09:18 +0800

    好

---
+++ java/com/shsxt/crm/controller/IndexController.java
@@ -0,0 +1,56 @@
+package com.shsxt.crm.controller;
+
+import com.shsxt.base.BaseController;
+import com.shsxt.crm.exceptions.ParamsException;
+import com.shsxt.crm.service.ModuleService;
+import com.shsxt.crm.service.PermissionService;
+import com.shsxt.crm.service.UserService;
+import com.shsxt.crm.utils.LoginUserUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class IndexController extends BaseController {
+
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private PermissionService permissionService;
+
+    @Resource
+    private ModuleService moduleService;
+
+    /**
+     * 登录页
+     * @return
+     */
+    @RequestMapping("index")
+    public String index(){
+        /*if(1==1){
+            throw  new ParamsException("参数异常...");
+        }*/
+        return "index";
+    }
+
+
+    /**
+     * 后端管理主页面
+     * @return
+     */
+    @RequestMapping("main")
+    public String main(HttpServletRequest request){
+        Integer userId = LoginUserUtil.releaseUserIdFromCookie(request);
+        List<String> permissions=permissionService.queryUserHasRolesHasPermissions(userId);
+        request.getSession().setAttribute("permissions",permissions);
+        request.getSession().setAttribute("modules",moduleService.queryUserHasRoleHasModuleDtos(userId));
+        request.setAttribute("user",userService.selectByPrimaryKey(userId));;
+        return "main";
+    }
+}
---
+++ java/com/shsxt/crm/controller/SaleChanceController.java
@@ -0,0 +1,82 @@
+package com.shsxt.crm.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.shsxt.base.BaseController;
+import com.shsxt.crm.annotaions.CrmLog;
+import com.shsxt.crm.annotaions.RequirePermission;
+import com.shsxt.crm.model.ResultInfo;
+import com.shsxt.crm.query.SaleChanceQuery;
+import com.shsxt.crm.service.SaleChanceService;
+import com.shsxt.crm.service.UserService;
+import com.shsxt.crm.utils.LoginUserUtil;
+import com.shsxt.crm.vo.SaleChance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("sale_chance")
+public class SaleChanceController extends BaseController {
+
+    @Resource
+    private SaleChanceService saleChanceService;
+
+    @Resource
+    private UserService userService;
+
+    @Autowired
+    private  HttpServletRequest request;
+
+    @RequestMapping("index")
+    @CrmLog(module = "营销机会",oper = "主页展示")
+    public String index(){
+        return "sale_chance";
+    }
+
+
+    @RequestMapping("list")
+    @ResponseBody
+    @RequirePermission(code = "101001")
+    @CrmLog(module = "营销管理",oper = "多条件查询")
+    public Map<String,Object> querySaleChancesByParams(SaleChanceQuery saleChanceQuery){
+        return  saleChanceService.querySaleChancesByParams(saleChanceQuery);
+    }
+
+
+    @RequestMapping("save")
+    @ResponseBody
+    @RequirePermission(code = "101002")
+    @CrmLog(module = "营销管理",oper = "添加")
+    public ResultInfo saveSaleChance(SaleChance saleChance){
+        saleChance.setCreateMan(userService.selectByPrimaryKey(LoginUserUtil.releaseUserIdFromCookie(request)).getTrueName());
+        saleChanceService.saveSaleChance(saleChance);
+        return success("机会数据添加成功");
+    }
+
+
+    @RequestMapping("update")
+    @ResponseBody
+    @RequirePermission(code = "101004")
+    @CrmLog(module = "营销管理",oper = "更新")
+    public ResultInfo updateSaleChance(SaleChance saleChance){
+        saleChanceService.updateSaleChance(saleChance);
+        return success("机会数据更新成功");
+    }
+
+    @RequestMapping("delete")
+    @ResponseBody
+    @RequirePermission(code = "101003")
+    @CrmLog(module = "营销管理",oper = "删除")
+    public ResultInfo deleteSaleChance(Integer[] ids){
+        saleChanceService.deleteBatch(ids);
+        return success("机会数据删除成功");
+    }
+
+}
